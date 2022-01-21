397a8717660efc33d3c58bd17eea71866e940ae4
Istiak <aistiak2078@gmail.com>
2022-01-21 18:05:50 +0600

    Autemeted changes

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -9,7 +9,9 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:connectivity_plus_web/connectivity_plus_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:flutter_facebook_auth_web/flutter_facebook_auth_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -20,7 +22,9 @@ void registerPlugins(Registrar registrar) {
   ConnectivityPlusPlugin.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FlutterFacebookAuthPlugin.registerWith(registrar);
   FluttertoastWebPlugin.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
